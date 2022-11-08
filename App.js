3b6789bba0f7d3207464d9768319989f52279e4a
LeeChiYun <carrick035@gmail.com>
2022-11-08 22:29:28 +0900

    first

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen";
+import DetailScreen from "./screens/DetailScreen";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { View, Button } from "react-native";
+
+function HomeScreen({ navigation }) {
+  return (
+    <View>
+      <Button title="Detail 열기" />
+    </View>
+  );
+}
+
+export default HomeScreen;
